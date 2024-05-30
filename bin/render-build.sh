2f735cb301a9b92fc6714f117857cb308cd0770d
emanuks <emanuels313@gmail.com>
2024-05-30 15:49:57 -0300

    fix: order

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-bundle exec rails stimulus:install
 bundle install
+bundle exec rails stimulus:install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
